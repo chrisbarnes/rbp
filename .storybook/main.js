320f777163d89c489529658b661908a66eadc290
Chris Barnes <barnes.chris@gmail.com>
2021-12-24 10:35:18 -0500

    Update sb config so tailwind works with it

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,14 +1,25 @@
 module.exports = {
-  "stories": [
+  stories: [
     "../stories/**/*.stories.mdx",
-    "../stories/**/*.stories.@(js|jsx|ts|tsx)"
+    "../stories/**/*.stories.@(js|jsx|ts|tsx)",
   ],
-  "addons": [
+  addons: [
     "@storybook/addon-links",
-    "@storybook/addon-essentials"
+    "@storybook/addon-essentials",
+    {
+      name: "@storybook/addon-postcss",
+      options: {
+        cssLoaderOptions: {
+          importLoaders: 1,
+        },
+        postcssLoaderOptions: {
+          implementation: require("postcss"),
+        },
+      },
+    },
   ],
-  "framework": "@storybook/react",
-  "core": {
-    "builder": "webpack5"
-  }
-}
+  framework: "@storybook/react",
+  core: {
+    builder: "webpack5",
+  },
+};
