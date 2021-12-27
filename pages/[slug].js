03b8910109b80f034391fcdd40b22f93c734f279
Chris Barnes <barnes.chris@gmail.com>
2021-12-27 11:09:16 -0500

    An example image component for pages

--- pages/[slug].js
+++ pages/[slug].js
@@ -1,10 +1,23 @@
 import { Layout } from "../components/Layout/Layout";
 import { getPages, getPageBySlug, getPageNavigation } from "../lib/api";
 import { documentToReactComponents } from "@contentful/rich-text-react-renderer";
+import Image from "next/image";
 
 export default function Page({ page, navigation }) {
   return (
     <Layout {...navigation}>
+      {page && page?.fields?.heroImage && (
+        <Image
+          alt={page?.fields?.heroImage?.fields?.title}
+          src={`https:${page?.fields?.heroImage?.fields?.file?.url}?fm=webp`} // Force webp as the default image format here
+          layout="responsive"
+          width={700}
+          height={475}
+          placeholder="blur"
+          blurDataURL={`https:${page?.fields?.heroImage?.fields?.file?.url}?fm=jpg&q=1`} // Use a super low quality jpg as the blur data url
+          priority
+        />
+      )}
       {page && <h1>{page?.fields?.title}</h1>}
       {page && documentToReactComponents(page?.fields?.mainContent)}
     </Layout>
