16b04dba66b8cbcf220a60d5ce28b884caa15b10
Chris Barnes <barnes.chris@gmail.com>
2021-12-26 20:54:56 -0500

    Add in a some test navigation stories and fix the homepage

--- stories/pages/home.stories.jsx
+++ stories/pages/home.stories.jsx
@@ -1,10 +1,17 @@
 import Home from "../../pages/index";
+import { navigation } from "../mocks/navigation";
 
 const HomeStory = {
   title: "Pages/Home",
   component: Home,
 };
 
+const homeProps = {
+  fields: {
+    title: "Regular Page",
+  },
+};
+
 export default HomeStory;
 
-export const HomePage = () => <Home />;
+export const HomePage = () => <Home navigation={navigation} page={homeProps} />;
