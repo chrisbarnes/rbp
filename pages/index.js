7d62e0fa44c1cd9c26cb830b64b2790bb4c389a6
Chris Barnes <barnes.chris@gmail.com>
2022-01-14 21:42:26 -0500

    Update the homepage to pull in the content from contentful

--- pages/index.js
+++ pages/index.js
@@ -1,22 +1,37 @@
 import { Layout } from "../components/Layout/Layout";
-import { getPageNavigation } from "../lib/api";
+import { getPageNavigation, getHomepage } from "../lib/api";
+import { documentToReactComponents } from "@contentful/rich-text-react-renderer";
+import Image from "next/image";
 
-export default function Home({ navigation, preview }) {
+export default function Home({ page, navigation, preview }) {
   return (
     <Layout {...navigation} isPreview={preview}>
-      <div className="container mx-auto">
-        <p>hello from the homepage</p>
-      </div>
+      {page && page?.fields?.heroImage && (
+        <Image
+          alt={page?.fields?.heroImage?.fields?.title}
+          src={`https:${page?.fields?.heroImage?.fields?.file?.url}?fm=webp`} // Force webp as the default image format here
+          layout="responsive"
+          width={700}
+          height={475}
+          placeholder="blur"
+          blurDataURL={`https:${page?.fields?.heroImage?.fields?.file?.url}?fm=jpg&q=1`} // Use a super low quality jpg as the blur data url
+          priority
+        />
+      )}
+      {page && <h1>{page?.fields?.title}</h1>}
+      {page && documentToReactComponents(page?.fields?.mainContent)}
     </Layout>
   );
 }
 
 export async function getStaticProps({ params, preview = false }) {
+  const page = await getHomepage(preview);
   const navigation = await getPageNavigation();
 
   return {
     props: {
       preview,
+      page: page ? page : null,
       navigation: navigation ? navigation : null,
     },
   };
