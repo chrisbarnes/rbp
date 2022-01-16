0371e9907bb559c283a4d85844d6f3b68495517f
Chris Barnes <barnes.chris@gmail.com>
2022-01-16 15:54:23 -0500

    Update select styling to match the text input

--- components/Forms/Select.js
+++ components/Forms/Select.js
@@ -1,17 +1,26 @@
 import React from "react";
+import { FieldContainer } from "./FieldContainer";
+import { FieldLabel } from "./FieldLabel";
+import { FieldError } from "./FieldError";
 
 export const Select = React.forwardRef((props, ref) => {
   return (
-    <>
-      <label htmlFor={props.id}>{props.label}</label>
-      <select id={props.id} ref={ref} {...props}>
+    <FieldContainer isError={props.isError}>
+      <FieldLabel id={props.id} text={props.label} />
+      <select
+        className="appearance-none px-0"
+        id={props.id}
+        ref={ref}
+        {...props}
+      >
         {props.options.map((option, index) => (
           <option key={`${option.name}-${index}`} value={option.value}>
             {option.name}
           </option>
         ))}
       </select>
-    </>
+      {props.isError && <FieldError error={props.error} />}
+    </FieldContainer>
   );
 });
 
