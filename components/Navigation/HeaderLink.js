5741e74e5e5b2b8c98659fd7fd39b0b6d8fb0a21
Chris Barnes <barnes.chris@gmail.com>
2022-01-29 20:21:22 -0500

    Add basic header logo and link components

---
+++ components/Navigation/HeaderLink.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Link from "next/link";
+
+export const HeaderLink = ({ link }) => {
+  return (
+    <span className="uppercase text-sm font-serif" key={`link-${link.text}`}>
+      <Link href={link.url}>
+        <a className="px-4 py-1">{link.text}</a>
+      </Link>
+    </span>
+  );
+};
