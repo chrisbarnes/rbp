9c610aa324e547c49a37a959b6edcda92039e7e5
Chris Barnes <barnes.chris@gmail.com>
2022-01-31 11:32:12 -0500

    Update header to use the headerimage component

--- components/Layout/Header.js
+++ components/Layout/Header.js
@@ -1,10 +1,15 @@
 import { HeaderLinks } from "../Navigation/HeaderLinks";
+import { HeaderImage } from "../Images/HeaderImage";
 
-export const Header = ({ items }) => {
+export const Header = ({ items, image, isStorybook }) => {
   return (
-    <div>
-      <HeaderLinks links={items} />
-      <p>image here eventually</p>
+    <div className="relative">
+      <div className="fixed top-0 w-full z-10">
+        <HeaderLinks links={items} />
+      </div>
+      <div className="absolute top-0 w-full h-screen">
+        <HeaderImage image={image} preserveUrl={isStorybook} />
+      </div>
     </div>
   );
 };
