283dac4ca9c3cd76ca50255bd6b48aea93d8fd4e
Chris Barnes <barnes.chris@gmail.com>
2022-01-30 21:56:49 -0500

    Add the social links to the footer

--- components/Layout/Footer.js
+++ components/Layout/Footer.js
@@ -1,19 +1,22 @@
 import { FooterLinks } from "../Navigation/FooterLinks";
 import { FooterLogo } from "../Navigation/FooterLogo";
+import { SocialLinks } from "../Social/SocialLinks";
 
 export const Footer = ({ items }) => {
   return (
     <footer>
       <div className="container mx-auto flex">
         <section className="basis-2/3 p-12 bg-grayPurple">
-          <div className="w-28 m-auto mb-12">
+          <div className="w-28 m-auto mb-10">
             <FooterLogo />
           </div>
           <p className="text-center text-xl font-serif w-2/3 m-auto mb-8">
             Rae Barnes Photography provides playful portraits and heirloom
             artwork for busy parents in the greater Philadelphia region.
           </p>
-          <p className="text-center">social links go here</p>
+          <div className="mb-8">
+            <SocialLinks />
+          </div>
           <FooterLinks links={items} />
         </section>
         <section className="basis-1/3 p-12 bg-lightPurple">
