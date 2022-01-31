26d385250a5679716b28bc40ac478491a8f43d74
Chris Barnes <barnes.chris@gmail.com>
2022-01-31 11:28:33 -0500

    Update config so next/image works in sb

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,4 +1,15 @@
 import "../styles/globals.css";
+import * as NextImage from "next/image";
+
+const OriginalNextImage = NextImage.default;
+
+// Set the unoptimized prop on next image components so they work in SB
+Object.defineProperty(NextImage, "default", {
+  configurable: true,
+  value: (props) => (
+    <OriginalNextImage {...props} unoptimized src={props.src} />
+  ),
+});
 
 export const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
