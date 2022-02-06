c11388d174fdaf20459d507b989d086b7fdd80fd
Chris Barnes <barnes.chris@gmail.com>
2022-02-05 20:38:25 -0500

    Smooth scroll for jump links

--- components/Navigation/JumpLink.js
+++ components/Navigation/JumpLink.js
@@ -1,10 +1,21 @@
-import React from "react";
+import { useRef } from "react";
 
 export const JumpLink = ({ text, section }) => {
+  const linkRef = useRef();
+  const handleClick = (event) => {
+    event.preventDefault();
+
+    const sectionToScrollTo = document.querySelector(event?.target?.hash);
+
+    sectionToScrollTo.scrollIntoView({ behavior: "smooth" });
+  };
+
   return (
     <a
-      className="font-sans uppercase text-plum text-sm px-4"
+      className="font-sans uppercase text-plum text-sm px-7 no-underline"
       href={`#${section}`}
+      ref={linkRef}
+      onClick={handleClick}
     >
       {text}
     </a>
