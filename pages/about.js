aa57c6344741ddb05f2a0ff3131210ce1f333e26
Chris Barnes <barnes.chris@gmail.com>
2022-02-06 22:16:27 -0500

    Adding callout boxes to about us

--- pages/about.js
+++ pages/about.js
@@ -1,9 +1,13 @@
+import Link from "next/link";
+import Image from "next/image";
 import { Layout } from "../components/Layout/Layout";
 import { getPageBySlug, getPageNavigation } from "../lib/api";
-import { documentToReactComponents } from "@contentful/rich-text-react-renderer";
 import { SectionIntro } from "../components/Typography/SectionIntro";
 import { JumpLinks } from "../components/Navigation/JumpLinks";
 import { ImageCallout } from "../components/Typography/ImageCallout";
+import { ContentfulRichText } from "../components/Typography/ContentfulRichText";
+import { Button } from "../components/Button/Button";
+import { CalloutBoxes } from "../components/Typography/CalloutBoxes";
 
 export default function Page({ page, navigation, preview }) {
   const jumpLinks = [
@@ -21,6 +25,24 @@ export default function Page({ page, navigation, preview }) {
     },
   ];
 
+  const values = [
+    {
+      heading: "Quality matters to me",
+      description:
+        "I've worked hard to hone my skills so that my work isn't just pretty on social media; it will look great in print.",
+    },
+    {
+      heading: "Craftsmanship matters to me",
+      description:
+        "We work with specialists so that each piece you order from me will be handmade by the best craftsmen in the country.",
+    },
+    {
+      heading: "Your family matters to me",
+      description:
+        "My calendar is limited so that I can best serve each and every family that commissions me to photograph them.",
+    },
+  ];
+
   return (
     <Layout
       {...navigation}
@@ -35,7 +57,7 @@ export default function Page({ page, navigation, preview }) {
       {page && (
         <>
           <SectionIntro heading={page?.fields?.title} headingType="h1">
-            {documentToReactComponents(page?.fields?.mainContent)}
+            <ContentfulRichText content={page?.fields?.mainContent} />
           </SectionIntro>
           <JumpLinks links={jumpLinks} />
           <ImageCallout
@@ -45,10 +67,46 @@ export default function Page({ page, navigation, preview }) {
               title: page?.fields?.imageCalloutImage?.fields?.title,
             }}
           />
-          <section id="about-rae">
-            <p>complex content section</p>
+          <section id="about-rae" className="max-w-4xl mx-auto py-32 px-5">
+            <ContentfulRichText
+              content={page?.fields?.secondaryContent}
+              altParagraph={(node, children) => (
+                <p className="text-xl mb-8">{children}</p>
+              )}
+              altImage={(node, children) => {
+                // Force webp as the default image format here
+                let imageSrc = `https:${node?.data?.target?.fields?.file?.url}?fm=webp`;
+
+                // Use a super low quality jpg as the blur data url
+                let blurSrc = `https:${node?.data?.target?.fields?.file?.url}?fm=jpg&q=1`;
+
+                return (
+                  <span className="max-w-sm float-right pl-7 pb-7">
+                    <Image
+                      alt={node?.data?.target?.fields?.title}
+                      src={imageSrc}
+                      layout="intrinsic"
+                      width={378}
+                      height={406}
+                      placeholder="blur"
+                      blurDataURL={blurSrc}
+                    />
+                  </span>
+                );
+              }}
+            />
+            <div className="text-center mt-16">
+              <Link
+                href={`/${page?.fields?.secondaryContentCallToActionLink?.fields?.slug}`}
+                passHref
+              >
+                <Button type="link" size="large">
+                  {page?.fields?.secondaryContentCallToActionText}
+                </Button>
+              </Link>
+            </div>
           </section>
-          <p>values section</p>
+          <CalloutBoxes items={values} />
           <section id="about-experience">
             <SectionIntro
               id="about-experience"
