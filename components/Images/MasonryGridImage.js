c679ba9738be8527d50927ed8dc1cbe404de91c0
Chris Barnes <barnes.chris@gmail.com>
2022-02-26 11:34:21 -0500

    Basic masonry grid compoent

---
+++ components/Images/MasonryGridImage.js
@@ -0,0 +1,34 @@
+import Image from "next/image";
+
+export const MasonryGridImage = ({ image, preserveUrl }) => {
+  if (!image) {
+    return null;
+  }
+
+  // Force webp as the default image format here
+  let imageSrc = `https:${image.url}?fm=webp`;
+
+  // Use a super low quality jpg as the blur data url
+  let blurSrc = `https:${image.url}?fm=jpg&q=1`;
+
+  // If preserve url is true (storybook), don't force this to be an absolute url.
+  // This is so we can serve an image easily out of the static storybook site.
+  if (preserveUrl) {
+    imageSrc = image.url;
+    blurSrc = image.url.split(".")[0] + "-low." + image.url.split(".")[1];
+  }
+
+  return (
+    <div className="h-full relative">
+      <Image
+        alt={image.title}
+        src={imageSrc}
+        layout="fill"
+        width={426}
+        height={426}
+        placeholder="blur"
+        blurDataURL={blurSrc}
+      />
+    </div>
+  );
+};
