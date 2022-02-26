5471494b1f389d9dcfb39bbae6fe17c207fd9598
Chris Barnes <barnes.chris@gmail.com>
2022-02-26 11:32:47 -0500

    Create the basic content block

---
+++ components/Typography/BasicContentBlock.js
@@ -0,0 +1,29 @@
+import { ContentfulRichText } from "./ContentfulRichText";
+import { Button } from "../Button/Button";
+import Link from "next/link";
+
+export const BasicContentBlock = ({ richText, linkSlug, linkText }) => {
+  return (
+    <div className="text-center">
+      <ContentfulRichText
+        content={richText}
+        altParagraph={(node, children) => (
+          <p className="text-xl mb-8">{children}</p>
+        )}
+        altH3={(node, children) => (
+          <h3
+            className="text-green text-4xl pb-6 mb-8 bg-bottom bg-no-repeat bg-[url('/images/horizontal-dots.svg')]"
+            style={{ backgroundSize: "176px 2px" }}
+          >
+            {children}
+          </h3>
+        )}
+      />
+      <Link href={`/${linkSlug}`} passHref>
+        <Button type="link" size="large">
+          {linkText}
+        </Button>
+      </Link>
+    </div>
+  );
+};
