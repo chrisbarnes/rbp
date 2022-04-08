eee201223e407bb476d80fe018f2902785dd10b4
Chris Barnes <barnes.chris@gmail.com>
2022-04-07 21:09:28 -0400

    Enable next router in sb

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,4 +1,5 @@
 import "../styles/globals.css";
+import { RouterContext } from "next/dist/shared/lib/router-context";
 import * as NextImage from "next/image";
 
 const OriginalNextImage = NextImage.default;
@@ -6,9 +7,7 @@ const OriginalNextImage = NextImage.default;
 // Set the unoptimized prop on next image components so they work in SB
 Object.defineProperty(NextImage, "default", {
   configurable: true,
-  value: (props) => (
-    <OriginalNextImage {...props} unoptimized src={props.src} />
-  ),
+  value: (props) => <OriginalNextImage {...props} unoptimized src={props.src} />,
 });
 
 export const parameters = {
@@ -18,5 +17,8 @@ export const parameters = {
       color: /(background|color)$/i,
       date: /Date$/,
     },
+  },
+  nextRouter: {
+    Provider: RouterContext.Provider,
   },
 };
