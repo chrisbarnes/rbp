ddd775374c73db7774a580cc0aaab099a9d0cb23
Chris Barnes <barnes.chris@gmail.com>
2022-04-08 22:18:17 -0400

    Pull quote responsive styling

--- components/Typography/PullQuote.js
+++ components/Typography/PullQuote.js
@@ -8,7 +8,7 @@ export const PullQuote = ({ quote, author }) => {
   return (
     <div className={styles.wrap}>
       <figure className="mx-auto max-w-4xl">
-        <blockquote className="text-4xl font-serif">{quote}</blockquote>
+        <blockquote className="text-2xl md:text-4xl font-serif">{quote}</blockquote>
         {author && <figcaption className={styles.author}>{author}</figcaption>}
       </figure>
     </div>
