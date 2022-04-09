9b94d69a1090728bc96c5ba2742bc8ba5811e0fa
Chris Barnes <barnes.chris@gmail.com>
2022-04-08 22:32:15 -0400

    Linked image grid small screen fixes

--- components/Images/LinkedImageGridWithContent.js
+++ components/Images/LinkedImageGridWithContent.js
@@ -2,25 +2,15 @@ import Link from "next/link";
 import { GridImage } from "./GridImage";
 import { ContentfulRichText } from "../Typography/ContentfulRichText";
 
-export const LinkedImageGridWithContent = ({
-  linkedImageGridItems,
-  isStorybook,
-}) => {
+export const LinkedImageGridWithContent = ({ linkedImageGridItems, isStorybook }) => {
   return (
     <div className="flex flex-wrap">
       {linkedImageGridItems &&
         linkedImageGridItems.map((linkedImageGridItem, index) => {
           if (linkedImageGridItem.image) {
             return (
-              <Link
-                href={linkedImageGridItem.link}
-                key={`linked-grid-image-${index}`}
-                passHref
-              >
-                <a
-                  href={linkedImageGridItem.link}
-                  className="w-1/3 aspect-square"
-                >
+              <Link href={linkedImageGridItem.link} key={`linked-grid-image-${index}`} passHref>
+                <a href={linkedImageGridItem.link} className="w-full md:w-1/3 aspect-square">
                   <GridImage
                     key={`grid-image-${index}`}
                     image={linkedImageGridItem.image}
@@ -35,16 +25,13 @@ export const LinkedImageGridWithContent = ({
           if (linkedImageGridItem.heading) {
             return (
               <div
-                className="w-1/3 aspect-square flex flex-col justify-center p-8 text-center relative z-10 before:content-[' '] before:absolute before:top-0 before:right-0 before:bottom-0 before:left-0 before:bg-grayPurple before:-z-10 before:scale-110"
+                className="md:w-1/3 aspect-square flex flex-col justify-center p-8 text-center relative z-10 before:content-[' '] before:absolute before:top-0 before:right-0 before:bottom-0 before:left-0 before:bg-grayPurple before:-z-10 before:scale-110"
                 key={`linked-grid-content-${index}`}
               >
                 <h2 className="mb-8">{linkedImageGridItem.heading}</h2>
                 <h3>{linkedImageGridItem.description}</h3>
                 <Link href={linkedImageGridItem.link} passHref>
-                  <a
-                    className="uppercase text-darkPurple font-bold no-underline"
-                    href={linkedImageGridItem.link}
-                  >
+                  <a className="uppercase text-darkPurple font-bold no-underline" href={linkedImageGridItem.link}>
                     {linkedImageGridItem.linkText}
                   </a>
                 </Link>
