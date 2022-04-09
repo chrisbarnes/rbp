cf37ab05c0d51dd57f79af739d1668ea801f12ae
Chris Barnes <barnes.chris@gmail.com>
2022-04-08 22:37:53 -0400

    Responsive styling for split section

--- components/Typography/SplitSection.js
+++ components/Typography/SplitSection.js
@@ -4,14 +4,14 @@ import { Button } from "../Button/Button";
 export const SplitSection = ({ heading, children, link, linkText }) => {
   return (
     <section className="max-w-4xl mx-auto mb-28">
-      <div className="flex">
+      <div className="flex flex-col md:flex-row">
         <p
-          className="text-green text-4xl text-right font-serif w-72 shrink-0 pr-12 mb-8"
+          className="text-green text-4xl text-center md:text-right font-serif w-72 mx-auto shrink-0 md:pr-12 mb-8"
           style={{ lineHeight: "2.8rem" }}
         >
           {heading}
         </p>
-        <div className="text-darkPurple text-2xl pl-12 leading-9 relative before:content-[' '] before:absolute before:bg-no-repeat before:bg-left-top before:bg-[url('/images/horizontal-dots.svg')] before:left-0 before:top-0 before:w-44 before:h-1 before:rotate-90 before:origin-left">
+        <div className="text-darkPurple text-2xl px-6 md:pr-0 md:pl-12 leading-9 relative before:content-[' '] before:absolute before:bg-no-repeat before:bg-left-top md:before:bg-[url('/images/horizontal-dots.svg')] before:left-0 before:top-0 before:w-44 before:h-1 before:rotate-90 before:origin-left">
           {children}
         </div>
       </div>
