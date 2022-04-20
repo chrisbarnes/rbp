35c8423eb90d7d2046e86012493340e382af3067
Chris Barnes <barnes.chris@gmail.com>
2022-04-19 21:18:04 -0400

    Attempt at setting the release for sentry

--- sentry.client.config.js
+++ sentry.client.config.js
@@ -12,6 +12,7 @@ Sentry.init({
   environment: SENTRY_ENV,
   // Adjust this value in production, or use tracesSampler for greater control
   tracesSampleRate: 1.0,
+  release: process.env.SENTRY_RELEASE,
   // ...
   // Note: if you want to override the automatic release value, do not set a
   // `release` value here - use the environment variable `SENTRY_RELEASE`, so
--- sentry.server.config.js
+++ sentry.server.config.js
@@ -5,11 +5,14 @@
 import * as Sentry from "@sentry/nextjs";
 
 const SENTRY_DSN = process.env.SENTRY_DSN || process.env.NEXT_PUBLIC_SENTRY_DSN;
+const SENTRY_ENV = process.env.SENTRY_ENV || process.env.NEXT_PUBLIC_SENTRY_ENV;
 
 Sentry.init({
   dsn: SENTRY_DSN,
+  environment: SENTRY_ENV,
   // Adjust this value in production, or use tracesSampler for greater control
   tracesSampleRate: 1.0,
+  release: process.env.SENTRY_RELEASE,
   // ...
   // Note: if you want to override the automatic release value, do not set a
   // `release` value here - use the environment variable `SENTRY_RELEASE`, so
